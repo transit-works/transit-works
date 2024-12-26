236ba1cf2d98443d3857454318b3ce4db86ef673
jeevano <53156459+jeevano@users.noreply.github.com>
2024-12-26 18:12:23 -0500

    Relocate geojson conversion (#9)

--- route-service/src/main.rs
+++ route-service/src/main.rs
@@ -1,20 +1,20 @@
 mod layers;
 mod gtfs;
 
-use layers::{grid_network::GridNetwork, road_network::RoadNetwork};
-use gtfs::raw_gtfs::GtfsDataSet;
-use gtfs::gtfs::Gtfs;
+use std::fs::File;
+use std::io::Write;
+
+use gtfs::{gtfs::Gtfs, geojson};
 
 fn main() {
     println!("Hello, world!");
-
-    let tmp = "toronto.db";
-    let grid = GridNetwork::load(tmp);
-    let road = RoadNetwork::load(tmp);
-
-    let gtfs = GtfsDataSet::from_path("/Users/jeevanopel/workspace/transit-works/scripts/toronto.db").unwrap();
+    let gtfs = Gtfs::from_path("/Users/jeevanopel/workspace/transit-works/scripts/toronto.db").unwrap();
     gtfs.print_stats();
-
-    let gtfs_enriched = Gtfs::from_path("/Users/jeevanopel/workspace/transit-works/scripts/toronto.db").unwrap();
-    gtfs_enriched.print_stats();
+    let features = geojson::get_all_features(&gtfs);
+    println!("There are {} features", features.len());
+    let geojson = geojson::convert_to_geojson(&features);
+    println!("Generated GeoJSON");
+    let mut file = File::create("data.geojson").unwrap();
+    file.write_all(serde_json::to_string_pretty(&geojson).unwrap().as_bytes())
+        .unwrap();
 }
